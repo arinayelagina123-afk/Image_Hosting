abeb0ce83aa48510b0d036969355b0bc5fcbf0de
arinayelagina123-afk <arina.yelagina.123@gmail.com>
2026-05-25 16:14:11 +0300

    Исправление docker-compose и синхронизация портов

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
-EXPOSE 3000
+EXPOSE 8000
 CMD ["python","app.py"]
--- app.py
+++ app.py
@@ -131,4 +131,4 @@ def get_image(filename):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3001, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
